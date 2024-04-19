f947e0098197e3125c8319e6cfda4a4561330244
adelajahalima <adelajahalima@gmail.com>
2024-04-19 23:47:44 +0100

    Edited file 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-exec {'install_flask':
-  command   => 'pip3 install flask==2.1.0',
-  provider => 'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
