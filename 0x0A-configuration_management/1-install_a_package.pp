f5c3d9d3dddc33f927e1bb913a1e87545232ca82
adelajahalima <adelajahalima@gmail.com>
2024-04-20 00:00:23 +0100

    Edited file 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 package { 'flask':
-  ensure   => 'present',
+  ensure   => 'latest',
   provider => 'pip3',
   install_options => ['--upgrade', 'Flask==2.1.0'],
 }
